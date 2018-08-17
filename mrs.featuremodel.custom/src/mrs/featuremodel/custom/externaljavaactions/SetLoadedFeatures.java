d5ce6b522c4d79e5749d16048f17f96979d85f79
Misha Strittmatter <strittmatter@kit.edu>
2018-08-17 16:37:12 +0200

    fix minor warning

--- mrs.featuremodel.custom/src/mrs/featuremodel/custom/externaljavaactions/SetLoadedFeatures.java
+++ mrs.featuremodel.custom/src/mrs/featuremodel/custom/externaljavaactions/SetLoadedFeatures.java
@@ -1,6 +1,5 @@
 package mrs.featuremodel.custom.externaljavaactions;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -18,29 +17,30 @@ import mrs.featuremodel.custom.util.MRSFeatureModelUtil;
 
 public class SetLoadedFeatures implements IExternalJavaAction {
 
-	@Override
-	public void execute(Collection<? extends EObject> selections, Map<String, Object> parameters) {
-		Layer layer = (Layer) parameters.get("layer");
-		Collection<Feature> features = (Collection<Feature>) parameters.get("features");
-		
-		ModularReferenceStructure mrs = layer.getModularReferenceStructure();
-		if (!MRSFeatureModelUtil.isProfileApplied(mrs.eResource()))
-			MRSFeatureModelUtil.applyMRSFeatureModelProfile(mrs.eResource());
-		
-		if (!MRSFeatureModelUtil.isStereotypeLayerFeatureApplied(layer))
-			MRSFeatureModelUtil.applyStereotypeLayerFeature(layer);
-		
-		StereotypeApplication stereotypeApplication = MRSFeatureModelUtil.getStereotypeApplicationLayerFeature(layer);
+    @Override
+    public void execute(Collection<? extends EObject> selections, Map<String, Object> parameters) {
+        Layer layer = (Layer) parameters.get("layer");
+        Collection<Feature> features = (Collection<Feature>) parameters.get("features");
 
-		List<EReference> refs = stereotypeApplication.getStereotype().getEReferences().stream().filter(r -> r.getEReferenceType().getInstanceClass().equals(Feature.class)).collect(Collectors.toList());
-		EReference FeatureReference = refs.get(0);
-		stereotypeApplication.eSet(FeatureReference, features);
+        ModularReferenceStructure mrs = layer.getModularReferenceStructure();
+        if (!MRSFeatureModelUtil.isProfileApplied(mrs.eResource()))
+            MRSFeatureModelUtil.applyMRSFeatureModelProfile(mrs.eResource());
 
-	}
+        if (!MRSFeatureModelUtil.isStereotypeLayerFeatureApplied(layer))
+            MRSFeatureModelUtil.applyStereotypeLayerFeature(layer);
 
-	@Override
-	public boolean canExecute(Collection<? extends EObject> selections) {
-		return true;
-	}
+        StereotypeApplication stereotypeApplication = MRSFeatureModelUtil.getStereotypeApplicationLayerFeature(layer);
+
+        List<EReference> refs = stereotypeApplication.getStereotype().getEReferences().stream().filter(r -> r.getEReferenceType().getInstanceClass().equals(Feature.class))
+                .collect(Collectors.toList());
+        EReference FeatureReference = refs.get(0);
+        stereotypeApplication.eSet(FeatureReference, features);
+
+    }
+
+    @Override
+    public boolean canExecute(Collection<? extends EObject> selections) {
+        return true;
+    }
 
 }
