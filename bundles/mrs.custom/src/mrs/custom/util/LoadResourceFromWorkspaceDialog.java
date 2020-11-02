b82a4ce0cf5318f3645e0b3d40a1c070818dc2c0
Sebastian Weber <uwmgy@student.kit.edu>
2020-11-02 10:48:04 +0100

    Migrated build process from buckminster to maven, eclipse oxygen to 4.10, java 8 to 11
    Used https://github.com/PalladioSimulator/Palladio-Build-BuckminsterToTychoMigrationPlugin for migration, updatesite built and tested successfully with Eclipse 4.10and Java 11

---
+++ bundles/mrs.custom/src/mrs/custom/util/LoadResourceFromWorkspaceDialog.java
@@ -0,0 +1,29 @@
+package mrs.custom.util;
+
+
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.edit.ui.action.LoadResourceAction.LoadResourceDialog;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+
+public class LoadResourceFromWorkspaceDialog extends LoadResourceDialog {
+
+	public LoadResourceFromWorkspaceDialog(Shell parent) {
+		super(parent);
+	}
+
+	public LoadResourceFromWorkspaceDialog(Shell parent, EditingDomain domain) {
+		super(parent, domain);
+	}
+	
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite composite = (Composite) super.createDialogArea(parent);
+        Composite buttonComposite = (Composite)composite.getChildren()[0];
+        buttonComposite.getChildren()[0].dispose();
+        return composite;
+		
+	}
+
+}
